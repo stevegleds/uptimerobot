076060f6dd4092fc672fd4e960229bf8d582878e
Steve Gledhill <stevegleds@users.noreply.github.com>
2017-04-11 13:59:22 +0100

    11 April 2017 Created processjson app to find friendly names. Works but only first 50 because of pagination limit Updated app.py to create separate files for each page - 4 in all Also, edited api call to also collect contacts to be notified Updated processjson with friendly names Next: need to delete contents of .txt file before appending.

--- processjson.py
+++ processjson.py
@@ -4,16 +4,21 @@ inputfilename = 'uptimerobot.json'
 inputfilename100 = 'uptimerobot100.json'
 inputfilename150 = 'uptimerobot150.json'
 inputfilename200 = 'uptimerobot200.json'
+outputfilename = 'uptimerobotmonitors.txt'
 
-def printmonitors(inputfilename):
+def printmonitors(inputfilename, outputfilename=outputfilename):
     with open(inputfilename, 'r') as inputfile:
         jsondata = inputfile.read()
+    outputfile = open(outputfilename, 'a')
     decoded = json.loads(jsondata)
     for check in range(len(decoded['monitors'])):
-        print("Monitor is:", decoded['monitors'][check]['friendly_name'])
+        monitorname = decoded['monitors'][check]['friendly_name']
+        print("Monitor is:", monitorname)
+        outputfile.write(monitorname + '\n')
     print(len(decoded['monitors']))
 
 printmonitors(inputfilename)
 printmonitors(inputfilename100)
 printmonitors(inputfilename150)
 printmonitors(inputfilename200)
+
