abeda3c304fab8d23bb916697d6440eff84036c3
Steve Gledhill <stevegleds@users.noreply.github.com>
2017-04-12 16:17:04 +0100

    12 April 2017 Added function to find who is being alerted by sms and their 'threshold' Uses contacts file Next step: produce in usable form linked to monitor name Possible just list: monitor then each contact in order of threshold. include the minutes

--- processjson.py
+++ processjson.py
@@ -1,10 +1,13 @@
 import json
+import csv
 
 inputfilename = 'uptimerobot.json'
 inputfilename100 = 'uptimerobot100.json'
 inputfilename150 = 'uptimerobot150.json'
 inputfilename200 = 'uptimerobot200.json'
 outputfilename = 'uptimerobotmonitors.txt'
+contactsfilename = 'contacts3.csv'
+smscontactsfilename = 'smscontacts.csv'
 
 def printmonitors(inputfilename, outputfilename=outputfilename):
     with open(inputfilename, 'r') as inputfile:
@@ -17,8 +20,40 @@ def printmonitors(inputfilename, outputfilename=outputfilename):
         outputfile.write(monitorname + '\n')
     print(len(decoded['monitors']))
 
+
+def getcontacts(contactsfilename):
+    uptimecontacts = {}
+    file = open(contactsfilename, newline='')
+    contactdata = csv.reader(file)
+    header=next(contactdata) # move to first data row
+    for row in contactdata:
+        uptimecontacts[row[1]] = row[0]
+    return uptimecontacts
+
+
+def printcontacts(inputfilename, contactsdictionary, outputfilename=outputfilename):
+    with open(inputfilename, 'r') as inputfile:
+        jsondata = inputfile.read()
+    outputfile = open(outputfilename, 'a')
+    decoded = json.loads(jsondata)
+    smscontacts = []
+    for check in range(len(decoded['monitors'])):
+        monitorname = decoded['monitors'][check]['friendly_name']
+        print("Monitor is:", monitorname)
+        contacts = decoded['monitors'][check]['alert_contacts']
+        smscontacts = [contact for contact in contacts if contact['type'] == 8]
+        # print(type(smscontacts), smscontacts)
+        for item in smscontacts:
+            print('first item is', type(item['value']), item['threshold'], contactsdictionary[item['value']])
+        print(type(contacts), contacts)
+    return smscontacts
+
 printmonitors(inputfilename)
 printmonitors(inputfilename100)
 printmonitors(inputfilename150)
 printmonitors(inputfilename200)
+
+uptimecontacts = getcontacts(contactsfilename)
+print(uptimecontacts)
+printcontacts(inputfilename, uptimecontacts, smscontactsfilename)
 
